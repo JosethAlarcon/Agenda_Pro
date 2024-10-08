60e801862d6a79b303c840a60ed2309fc3c32c55
Joseth Alarcon <joseth.30.17@gmail.com>
2024-10-08 20:41:38 -0300

    Esta todo listo hasta llegar a la interfaz del formulario de servicio, (Falta darle funcionalidad al formulario)

--- app/src/main/java/com/josethcodedev/agenda_pro/Menu_act.java
+++ app/src/main/java/com/josethcodedev/agenda_pro/Menu_act.java
@@ -1,6 +1,9 @@
 package com.josethcodedev.agenda_pro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -12,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Menu_act extends AppCompatActivity {
 
+    //Crear variables para los botones
+    public Button btn_pasajero, btn_mudanza, btn_encomienda;
+
     private ViewFlipper vf;
     private int[] image = {R.drawable.imgtaxi, R.drawable.imgmudanza, R.drawable.imgencomiendan};
     @Override
@@ -20,12 +26,38 @@ public class Menu_act extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
 
+        btn_pasajero = findViewById(R.id.btn_pasajero);
+        btn_mudanza = findViewById(R.id.btn_mudanza);
+        btn_encomienda = findViewById(R.id.btn_encomienda);
+
         vf = (ViewFlipper) findViewById(R.id.slider);
+
+        btn_pasajero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Menu_act.this, TransOpciones_act.class));
+            }
+        });
+
+        btn_mudanza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Menu_act.this, TransOpciones_act.class));
+            }
+        });
+
+        btn_encomienda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Menu_act.this, TransOpciones_act.class));
+            }
+        });
 
         for(int i=0; i<image.length; i++)
         {
             flip_image(image[i]);
         }
+
     }
     public void flip_image(int i)
     {
@@ -38,4 +70,5 @@ public class Menu_act extends AppCompatActivity {
         vf.setInAnimation(this, android.R.anim.slide_in_left);
         vf.setOutAnimation(this, android.R.anim.slide_out_right);
     }
+
 }
